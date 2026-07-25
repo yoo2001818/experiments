23f80c594bd4c7e0190737673d1b76231779c2db
Duknam Yoo <yoo2001818@gmail.com>
2026-07-26 07:17:09 +0900

    Add logical_op.cpp

--- minidb/include/minidb/logical_op.hpp
+++ minidb/include/minidb/logical_op.hpp
@@ -1,18 +1,43 @@
 #pragma once
 
+#include "minidb/ast.hpp"
 #include "minidb/table.hpp"
+
+#include <cstdint>
+#include <string>
+#include <vector>
 
 namespace minidb {
 
-struct FrameLayout {};
+struct FrameColumnDescriptor {
+  std::uint32_t index;
+  std::string name;
+  ExprPtr expr;
+};
+
+using FrameDescriptor = std::vector<FrameColumnDescriptor>;
 
 class LogicalOp {
 public:
+  virtual ~LogicalOp() = default;
+
+  virtual FrameDescriptor get_frame() = 0;
 };
 
 class ScanLogicalOp : public LogicalOp {
 public:
-  ScanLogicalOp(Table &table);
+  explicit ScanLogicalOp(Table &table);
+
+  FrameDescriptor get_frame() override;
+
+private:
+  struct ScanFrameColumnDescriptor {
+    FrameColumnDescriptor descriptor;
+    std::uint32_t column_index;
+  };
+
+  Table *table_;
+  std::vector<ScanFrameColumnDescriptor> frame_;
 };
 
 } // namespace minidb

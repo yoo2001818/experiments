63eb3fa411cee84707382d46ef21180a972032fa
Duknam Yoo <yoo2001818@gmail.com>
2026-07-26 17:14:31 +0900

    Add basic logical ops

--- minidb/include/minidb/logical_op.hpp
+++ minidb/include/minidb/logical_op.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include "minidb/ast.hpp"
+#include "minidb/enum.hpp"
 #include "minidb/table.hpp"
 
 #include <cstdint>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -13,6 +15,7 @@ struct FrameColumnDescriptor {
   std::uint32_t index;
   std::string name;
   ExprPtr expr;
+  bool visible;
 };
 
 using FrameDescriptor = std::vector<FrameColumnDescriptor>;
@@ -23,6 +26,8 @@ public:
 
   virtual FrameDescriptor get_frame() = 0;
 };
+
+using LogicalOpPtr = std::shared_ptr<LogicalOp>;
 
 class ScanLogicalOp : public LogicalOp {
 public:
@@ -38,6 +43,45 @@ private:
 
   Table *table_;
   std::vector<ScanFrameColumnDescriptor> frame_;
+};
+
+class FilterLogicalOp : public LogicalOp {
+public:
+  explicit FilterLogicalOp(LogicalOpPtr parent, ExprPtr expr);
+
+  FrameDescriptor get_frame() override;
+
+private:
+  LogicalOpPtr parent_;
+  ExprPtr expr_;
+};
+
+class ProjectLogicalOp : public LogicalOp {
+public:
+  explicit ProjectLogicalOp(LogicalOpPtr parent, FrameDescriptor &frame);
+
+  FrameDescriptor get_frame() override;
+
+private:
+  LogicalOpPtr parent_;
+  FrameDescriptor frame_;
+};
+
+class SortLogicalOp : public LogicalOp {
+public:
+  explicit SortLogicalOp(LogicalOpPtr parent,
+                         std::vector<OrderByTerm> &order_by);
+
+  FrameDescriptor get_frame() override;
+
+private:
+  struct SortTermDescriptor {
+    ExprPtr expr;
+    SortDirection direction;
+  };
+
+  LogicalOpPtr parent_;
+  std::vector<SortTermDescriptor> terms_;
 };
 
 } // namespace minidb
